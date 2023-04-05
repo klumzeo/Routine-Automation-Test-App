3dc2ecf9c9179e0c801d13990ca45671b53fdafe
Andrey Katsuba <79650153+klumzeo@users.noreply.github.com>
2023-04-05 23:46:54 +0300

    added filtration

--- force-app/main/default/lwc/account_gallery/account_gallery.js
+++ force-app/main/default/lwc/account_gallery/account_gallery.js
@@ -1,3 +1,34 @@
 import { LightningElement } from 'lwc';
 
-export default class account_gallery extends LightningElement {}
+import getAccount from '@salesforce/apex/AccountGalleryController.getAccountByType';
+
+export default class account_gallery extends LightningElement {
+
+    accounts = [];
+    accountsToDisplay = [];
+    accountTypes = [];
+
+    connectedCallback() {
+        getAccount()
+            .then(result => {
+                console.log('OUTPUT : ', result);
+                if (result) {
+                    this.accountTypes = result.accountTypes;
+                    this.accountsToDisplay = result.accountList;
+                    this.accounts = result.accountList;
+                }
+            })
+    }
+
+    handleTypeChanged(event) {
+        if (event && event.detail) {
+            try {
+                this.accountsToDisplay = event.detail.value == 'All'
+                    ? this.accounts
+                    : this.accounts.filter(account => account.Type == event.detail.value);
+            } catch (error) {
+                console.log('OUTPUT : error', error);
+            }
+        }
+    }
+}
--- force-app/main/default/lwc/account_type_selector/account_type_selector.js
+++ force-app/main/default/lwc/account_type_selector/account_type_selector.js
@@ -1,3 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class account_type_selector extends LightningElement {}
+export default class account_type_selector extends LightningElement {
+    value = null;
+    _options = [];
+
+    @api
+    get options() {
+        return this._options;
+    };
+
+    set options(value) {
+        this._options = value;
+        this.value = (value && value[0]) ? value[0].value : null;
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+
+        this.dispatchEvent(
+            new CustomEvent(
+                'typechanged',
+                {
+                    detail: {
+                        value: this.value
+                    }
+                }
+            )
+        );
+    }
+
+}
